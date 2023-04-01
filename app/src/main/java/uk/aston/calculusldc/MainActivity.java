eeaddb3084b9008e01f0f8a5e65a378de0057899
Abideet <51164427+Abideet@users.noreply.github.com>
2023-04-01 18:36:57 +0100

    Final Update

--- app/src/main/java/uk/aston/calculusldc/MainActivity.java
+++ app/src/main/java/uk/aston/calculusldc/MainActivity.java
@@ -48,7 +48,8 @@ import uk.aston.calculusldc.root.differentiation.SavedFragment;
 public class MainActivity extends AppCompatActivity
 {
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
